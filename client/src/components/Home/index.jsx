69e2aa2e16c0306cbdff30d9c9822cd88eee63ac
alexmonirji <alexmonirji@gmail.com>
2018-03-07 15:33:40 -0800

    [ops] - dropdown friends

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -11,17 +11,22 @@ let slingId;
 
 class Home extends Component {
   state = {
+    allFriends: [],
+    selectedFriend: {},
     allChallenges: [],
     selectedChallenge: {}
-   }
+  }
 
-   async componentDidMount() {
+  async componentDidMount() {
     const id = localStorage.getItem('id');
-    const { data } = await axios.get(`http://localhost:3396/api/usersChallenges/${id}`);
+    var { data } = await axios.get(`http://localhost:3396/api/usersChallenges/${id}`);
     if (data.rows.length) {
       this.setState({ allChallenges: data.rows, selectedChallenge: data.rows[0] });
     }
-   }
+
+    var { data } = await axios.get(`http://localhost:3396/api/friends/fetchAllFriends/${id}`);
+    console.log(data);
+  }
 
   randomSlingId = () => {
     slingId = `${randomstring.generate()}`;
@@ -47,12 +52,30 @@ class Home extends Component {
     this.setState({ selectedChallenge: value });
   }
 
+  handleFriendSelect = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    this.setState({ selectedFriend: value });
+  }
+
   render() {
     return (
       <div className="landing-page-container">
         <Logo
           className="landing-page-logo"
         />
+        <br />
+        <select onChange={(e) => this.handleFriendSelect(e)}>
+          {this.state.allFriends.map(friend => {
+            return (
+            <option
+              value={JSON.stringify(friend)}
+            >
+              {friend.username}
+            </option>)
+          }
+          )}
+        </select>
         <br />
         <select onChange={(e) => this.handleChallengeSelect(e)}>
           {this.state.allChallenges.map(challenge => {
