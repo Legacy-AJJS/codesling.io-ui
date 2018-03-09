2182d07f3a4432f6bb51fd7d18c4d9304d7b2d2b
alexmonirji <alexmonirji@gmail.com>
2018-03-09 15:43:24 -0800

    [ops] - dynamically add input and output fields

--- client/src/components/Challenge/AddChallenge/index.jsx
+++ client/src/components/Challenge/AddChallenge/index.jsx
@@ -8,12 +8,11 @@ import Logo from '../../globals/Logo';
 import './Auth.css';
 
 class AddChallenge extends Component {
-  state = { 
-    title: '',
-    content: '',
-    input: '',
-    output: '',
-    difficulty: null
+  state = {
+    inputs: [],
+    outputs: [],
+    numInputs: 1,
+    numOutputs: 1
    }
 
   submitChallenge = async (e) => {
@@ -43,6 +42,37 @@ class AddChallenge extends Component {
   handleChallengeInput = (event) => {
     const { name, value } = event.target;
     this.setState({ [name]: value });
+    console.log(this.state);
+  }
+
+  handleAddInputClick = (event) => {
+    event.preventDefault();
+    
+    this.state.inputs.push(
+      <Input
+        name={`input${this.state.numInputs}`}
+        placeholder="enter input"
+        onChange={this.handleChallengeInput}
+        key={this.state.numInputs}
+      />
+    );
+
+    this.setState({ inputs: this.state.inputs, numInputs: this.state.numInputs + 1 });
+  }
+
+  handleAddOutputClick = (event) => {
+    event.preventDefault();
+    
+    this.state.outputs.push(
+      <Input
+        name={`output${this.state.numOutputs}`}
+        placeholder="enter expected output"
+        onChange={this.handleChallengeInput}
+        key={this.state.numOutputs}
+      />
+    );
+
+    this.setState({ outputs: this.state.outputs, numOutputs: this.state.numOutputs + 1 });
   }
 
   goToHome = () => {
@@ -57,42 +87,52 @@ class AddChallenge extends Component {
           className="landing-page-logo"
         />
         <form className="auth-form">
+          <Button
+            backgroundColor="red"
+            color="white"
+            text="Add Input"
+            onClick={(e) => this.handleAddInputClick(e)}
+          />
+          <Button
+            backgroundColor="red"
+            color="white"
+            text="Add Expected Output"
+            onClick={(e) => this.handleAddOutputClick(e)}
+          />
           <Input
-            name='title'
-            type='title'
-            placeholder={'enter title'}
+            name="title"
+            placeholder={"enter title"}
             onChange={this.handleChallengeInput}
-            />
+          />
           <Input
-            name='content'
-            type='content'
-            placeholder={'enter content'}
+            name="content"
+            placeholder={"enter content"}
             onChange={this.handleChallengeInput}
-            />
+          />
           <Input 
-            name='difficulty'
-            type='difficulty'
-            placeholder={'enter your difficulty'}
+            name="difficulty"
+            type="number"
+            placeholder={"enter your difficulty"}
             onChange={this.handleChallengeInput}
-            />
-          <Input 
-            name='input'
-            type='input'
-            placeholder={'enter input'}
+          />
+          <Input
+            name="input0"
+            placeholder="enter input"
             onChange={this.handleChallengeInput}
-            />
-          <Input 
-            name='output'
-            type='output'
-            placeholder={'enter expected output'}
+          />
+          {this.state.inputs.map((input) => input)}
+          <Input
+            name="output0"
+            placeholder="enter expected output"
             onChange={this.handleChallengeInput}
-            />
+          />
+          {this.state.outputs.map((output) => output)}
           <Button
             backgroundColor="red"
             color="white"
             text="Add Challenge"
             onClick={(e) => this.submitChallenge(e)}
-            />
+          />
         </form>
       </div>
     );
